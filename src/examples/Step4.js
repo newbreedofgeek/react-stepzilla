7ef9462177784624639a7ec5d6ecffe25789cbe6
newbreedofgeek <paul.mark@gmail.com>
2017-02-21 10:26:42 +1100

    WIP - HOC validation

--- src/examples/Step4.js
+++ src/examples/Step4.js
@@ -1,102 +1,94 @@
 'use strict';
 
 import React, { Component, PropTypes } from 'react';
-import Promise from 'promise';
+import validation from 'react-validation-mixin';
+import strategy from 'joi-validation-strategy';
+import Joi from 'joi';
 
-export default class Step4 extends Component {
-  constructor(props) {
-    super(props);
+class Step4 extends Component {
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      saving: false
+        this.state = {
+            email: ''
+        };
+
+        this.validatorTypes = {
+          email: Joi.string().email().required()
+        };
+
+        this.getValidatorData = this.getValidatorData.bind(this);
+        this.renderHelpText = this.renderHelpText.bind(this);
+    }
+
+    getValidatorData() {
+        return {
+            email: this.refs.email.value,
+        }
     };
 
-    this.isValidated = this._isValidated.bind(this); // provide a public isValidated() method. Here its bound to a private _isValidated method
-  }
+    onChange(e) {
+        let newState = {};
+        newState[e.target.name] = e.target.value;
+        this.setState(newState);
+    }
 
-  componentDidMount() {}
+    renderHelpText(message, id) {
+        return (<div className="val-err-tooltip" key={id}><span>{message}</span></div>);
+    };
 
-  componentWillUnmount() {}
+    render() {
+        // explicit class assigning based on validation
+        let notValidClasses = {};
+        notValidClasses.emailCls = this.props.isValid('email') ?
+            'no-error col-md-8' : 'has-error col-md-8';
 
-  // This review screen had the 'Save' button, on clicking this is called
-  _isValidated() {
-    // typically this method needs to return true or false (to indicate if the local forms are validated, so StepZilla can move to the next step),
-    // but in this example we simulate an ajax request which is async. In the case of async validation or server saving etc. return a Promise and StepZilla will wait
-    // ... for the resolve() to work out if we can move to the next step
-    // So here are the rules:
-    // ~~~~~~~~~~~~~~~~~~~~~~~~
-    // SYNC action (e.g. local JS form validation).. if you return:
-    // true/undefined: validation has passed. Move to next step.
-    // false: validation failed. Stay on current step
-    // ~~~~~~~~~~~~~~~~~~~~~~~~
-    // ASYNC return (server side validation or saving data to server etc).. you need to return a Promise which can resolve like so:
-    // resolve(): validation/save has passed. Move to next step.
-    // reject(): validation/save failed. Stay on current step
+        return (
+            <div className="step step4">
+                <div className="row">
+                    <form id="Form" className="form-horizontal">
+                        <div className="form-group">
+                            <label className="col-md-12 control-label">
+                                <h1>Step 4: Enter your emergency contacts details:</h1>
+                            </label>
+                        </div>
+                        <div className="form-group col-md-12">
+                            <label className="control-label col-md-4">
+                                Email
+                            </label>
+                            <div className={notValidClasses.emailCls}>
+                                <input
+                                    ref="email"
+                                    autoComplete="off"
+                                    type="email"
+                                    placeholder="john.smith@example.com"
+                                    className="form-control"
+                                    name="email"
+                                    value={this.state.email}
+                                    required
+                                    onBlur={this.props.handleValidation('email')}
+                                    onChange={this.onChange.bind(this)}
+                                />
 
-    this.setState({
-      saving: true
-    });
-
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        this.setState({
-          saving: true
-        });
-
-        this.props.updateStore({savedToCloud: true});  // Update store here (this is just an example, in reality you will do it via redux or flux)
-
-        // call resolve() to indicate that server validation or other aync method was a success.
-        // ... only then will it move to the next step. reject() will indicate a fail
-        resolve();
-        // reject(); // or reject
-      }, 5000);
-    });
-  }
-
-  jumpToStep(toStep) {
-    // We can explicitly move to a step (we -1 as its a zero based index)
-    this.props.jumpToStep(toStep-1); // The StepZilla library injects this jumpToStep utility into each component
-  }
-
-  render() {
-    const savingCls = this.state.saving ? 'saving col-md-12 show' : 'saving col-md-12 hide';
-
-    return (
-      <div className="step step4 review">
-        <div className="row">
-          <form id="Form" className="form-horizontal">
-            <div className="form-group">
-              <label className="col-md-12 control-label">
-                <h1>Step 4: Review your Details and 'Save'</h1>
-              </label>
+                                {this.props.getValidationMessages('email').map(this.renderHelpText)}
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div className="form-group">
-              <div className="col-md-12 control-label">
-                <div className="col-md-12 txt">
-                  <div className="col-md-4">
-                    Gender
-                  </div>
-                  <div className="col-md-4">
-                    {this.props.getStore().gender}
-                  </div>
-                </div>
-                <div className="col-md-12 txt">
-                  <div className="col-md-4">
-                    Email
-                  </div>
-                  <div className="col-md-4">
-                    {this.props.getStore().email}
-                  </div>
-                </div>
-                <div className="col-md-12 eg-jump-lnk">
-                  <a href="#" onClick={() => this.jumpToStep(1)}>e.g. showing how we use the jumpToStep method helper method to jump back to step 1</a>
-                </div>
-                <h2 className={savingCls}>Saving to Cloud, pls wait (by the way, we are using a Promise to do this :)...</h2>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
-    )
-  }
+        )
+    }
 }
+
+Step4.propTypes = {
+    errors: PropTypes.object,
+    validate: PropTypes.func,
+    isValid: PropTypes.func,
+    handleValidation: PropTypes.func,
+    getValidationMessages: PropTypes.func,
+    clearValidations: PropTypes.func,
+    getStore: PropTypes.func,
+    updateStore: PropTypes.func,
+};
+
+export default validation(strategy)(Step4);
--- src/main.js
+++ src/main.js
@@ -225,8 +225,12 @@ export default class StepZilla extends Component {
       proceed = true;
     }
     else {
-      // if its a form component, it should have implemeted a public isValidated class (also pure componenets wont even have refs - i.e. a empty object). If not then continue
-      if (Object.keys(this.refs).length == 0 || typeof this.refs.activeComponent.isValidated == 'undefined') {
+      if (this.props.hocValidationPredicate != null && this.props.hocValidationAppliedTo.length > 0 && this.props.hocValidationAppliedTo.indexOf(this.state.compState) > -1) {
+        // the user is using a higer order component (HOC) for validation (e.g react-validation-mixin), this wraps the StepZilla steps as per hocValidationAppliedTo, so use the provided predicate method to determine if the HOC validation passes/fails
+        proceed = this.props.hocValidationPredicate(this.refs.activeComponent);
+      }
+      else if (Object.keys(this.refs).length == 0 || typeof this.refs.activeComponent.isValidated == 'undefined') {
+        // if its a form component, it should have implemeted a public isValidated class (also pure componenets wont even have refs - i.e. a empty object). If not then continue
         proceed = true;
       }
       else if (skipValidationExecution) {
@@ -323,5 +327,7 @@ StepZilla.defaultProps = {
   dontValidate: false,
   preventEnterSubmission: false,
   startAtStep: 0,
-  nextTextOnFinalActionStep: "Next"
+  nextTextOnFinalActionStep: "Next",
+  hocValidationAppliedTo: [],
+  hocValidationPredicate: null
 };
