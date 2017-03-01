23ae9d06b6b1444b33b593786698db31e3da400b
newbreedofgeek <paul.mark@gmail.com>
2017-03-02 09:58:10 +1100

    updated gitignore and txt fixes for step1

--- src/examples/Step1.js
+++ src/examples/Step1.js
@@ -30,11 +30,21 @@ export default class Step1 extends Component {
                 <div className="col-md-12">
                   <div className="col-md-6">
                     <h3>This example uses this custom config (which overwrite the default config):</h3>
-                    <code>preventEnterSubmission=true<br />nextTextOnFinalActionStep="Save"</code>
+                    <code>preventEnterSubmission=true<br />nextTextOnFinalActionStep="Save"<br />hocValidationAppliedTo=[3]</code>
                   </div>
                   <div className="col-md-6">
                     <h3>The default config settings are...</h3>
-                    <code>showSteps=true<br />showNavigation=true<br />stepsNavigation=true<br />prevBtnOnLastStep=true<br />dontValidate=false<br />preventEnterSubmission=false<br />startAtStep=0<br />nextTextOnFinalActionStep='Next'</code>
+                    <code>
+                      showSteps=true<br />
+                      showNavigation=true<br />
+                      stepsNavigation=true<br />
+                      prevBtnOnLastStep=true<br />
+                      dontValidate=false<br />
+                      preventEnterSubmission=false<br />
+                      startAtStep=0<br />
+                      nextTextOnFinalActionStep='Next'<br />
+                      hocValidationAppliedTo: []
+                    </code>
                   </div>
                 </div>
               </div>
